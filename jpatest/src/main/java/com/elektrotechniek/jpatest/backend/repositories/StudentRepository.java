e5d817e6ee540a232f506c750eee3e9e5a3143ca
Dinesh H <dinesh_harkhoe@hotmail.com>
2020-12-20 20:36:05 -0300

    added "select name from studno"

--- jpatest/src/main/java/com/elektrotechniek/jpatest/backend/repositories/StudentRepository.java
+++ jpatest/src/main/java/com/elektrotechniek/jpatest/backend/repositories/StudentRepository.java
@@ -8,8 +8,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-    @Query(value = "SELECT * FROM elektrotechniek.student WHERE (naam LIKE CONCAT('%', :search, '%')" +
-            "OR achternaam LIKE CONCAT('%', :search, '%')" +
-            "OR studentennummer LIKE CONCAT(:search, '%'))", nativeQuery = true)
+    @Query(value = "SELECT * FROM elektrotechniek.student WHERE (CONCAT (naam, ' ', achternaam) LIKE CONCAT('%', :search, '%')" +
+            " OR studentennummer LIKE CONCAT(:search, '%'))", nativeQuery = true)
     List<Student> search(@Param("search") String searchText);
+
+    @Query(value = "SELECT student.studentennummer FROM elektrotechniek.student WHERE " +
+            "CONCAT (naam, ' ', achternaam) LIKE CONCAT('%', :naam, '%')", nativeQuery = true)
+    List<Integer> selectParticularStudNo(@Param("naam") String naam);
 }
