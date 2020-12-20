682f300da0a9b8bf507254ec5ebfac4989afa823
Dinesh H <dinesh_harkhoe@hotmail.com>
2020-12-20 20:36:51 -0300

    search by name ipv number

--- jpatest/src/main/java/com/elektrotechniek/jpatest/backend/controllers/RapportController.java
+++ jpatest/src/main/java/com/elektrotechniek/jpatest/backend/controllers/RapportController.java
@@ -2,6 +2,7 @@ package com.elektrotechniek.jpatest.backend.controllers;
 
 import com.elektrotechniek.jpatest.backend.Rapport;
 import com.elektrotechniek.jpatest.backend.repositories.RapportRepository;
+import com.elektrotechniek.jpatest.backend.repositories.StudentRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,16 +10,27 @@ import java.util.List;
 
 @RestController
 public class RapportController {
+    private List<Integer> integerBuffer;
+    private String studNoBuffer;
     private final RapportRepository rapportRepository;
+    private final StudentRepository studentRepository;
 
-    RapportController(RapportRepository rapportRepository){
+    RapportController(RapportRepository rapportRepository, StudentRepository studentRepository){
         this.rapportRepository = rapportRepository;
+        this.studentRepository = studentRepository;
     }
 
     @GetMapping("/rapport")
-    List<Rapport> allRapport(@RequestParam(value = "student", required = false, defaultValue = "") String studentNum,
-                             @RequestParam(value = "jaar", required = false, defaultValue = "") String jaar){
-        return rapportRepository.rapportByStudentAndYear(studentNum, jaar);
+    List<Rapport> allRapport(@RequestParam(value = "student", required = false, defaultValue = "") String studentNaam,
+                             @RequestParam(value = "jaar", required = false, defaultValue = "") String jaar,
+                             @RequestParam(value = "vak", required = false, defaultValue = "") String vak){
+        integerBuffer = studentRepository.selectParticularStudNo(studentNaam);
+        if(integerBuffer.size()>1){
+            studNoBuffer = "";
+        } else{
+            studNoBuffer = integerBuffer.get(0).toString();
+        }
+        return rapportRepository.rapportByStudentAndYear(studNoBuffer, jaar, vak);
     }
 
 
