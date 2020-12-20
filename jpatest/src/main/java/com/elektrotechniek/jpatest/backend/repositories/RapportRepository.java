9734ee4e7b7695ec410094430a2693b5417323e3
Dinesh H <dinesh_harkhoe@hotmail.com>
2020-12-20 16:39:42 -0300

    added selectrapport by student

--- jpatest/src/main/java/com/elektrotechniek/jpatest/backend/repositories/RapportRepository.java
+++ jpatest/src/main/java/com/elektrotechniek/jpatest/backend/repositories/RapportRepository.java
@@ -2,14 +2,15 @@ package com.elektrotechniek.jpatest.backend.repositories;
 
 import com.elektrotechniek.jpatest.backend.Rapport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RapportRepository extends JpaRepository<Rapport, Integer> {
+    @Query(value = "SELECT * FROM elektrotechniek.rapport WHERE student_studentennummer = :num", nativeQuery = true)
+    List<Rapport> rapportByStudId(@Param("num") Integer studNummer);
+
+    //@Query(value = "SELECT * FROM elektrotechniek.rapport WHERE student_studentennummer = :num ")
 
 }
-
-
-/*
- * select * from elektrotechniek.rapport where datum = any (
- *	select max(datum) as datum from elektrotechniek.rapport group by vak_idvak
- * )
- */
