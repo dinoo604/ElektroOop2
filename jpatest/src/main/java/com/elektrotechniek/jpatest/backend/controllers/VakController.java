a11546c40042e70b7bcc46989af7b50d792c84d2
Dinesh H <dinesh_harkhoe@hotmail.com>
2020-12-20 16:30:31 -0300

    Update VakController.java
    added editVak & deleteVak

--- jpatest/src/main/java/com/elektrotechniek/jpatest/backend/controllers/VakController.java
+++ jpatest/src/main/java/com/elektrotechniek/jpatest/backend/controllers/VakController.java
@@ -1,12 +1,11 @@
 package com.elektrotechniek.jpatest.backend.controllers;
 
 import com.elektrotechniek.jpatest.backend.Vak;
+import com.elektrotechniek.jpatest.backend.exceptions.VakNotFoundException;
 import com.elektrotechniek.jpatest.backend.repositories.VakRepository;
-import com.elektrotechniek.jpatest.backend.exceptions.StudentNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 @RestController
@@ -25,6 +24,32 @@ public class VakController {
     @GetMapping("/vak/{id}")
     Vak oneVak(@PathVariable Integer id){
         return vakRepository.findById(id).
-                orElseThrow(()-> new StudentNotFoundException(id));
+                orElseThrow(()-> new VakNotFoundException(id));
+    }
+
+    @PostMapping("/vak")
+    Vak newVak(@RequestBody Vak newVak){
+        return vakRepository.save(newVak);
+    }
+
+
+    @PutMapping("/vak/{id}")
+    Vak editVak(@RequestBody Vak newVak, @PathVariable Integer id){
+        return vakRepository.findById(id).map(
+                vak-> {
+                    vak.setVak_naam(newVak.getVak_naam());
+                    vak.setSemester(newVak.getSemester());
+                    vak.setStudiepunten(newVak.getStudiepunten());
+                    vak.setVak_orientatie(newVak.getVak_orientatie());
+                    return vakRepository.save(vak);
+                }).orElseGet(() -> {
+                    newVak.setIdvak(id);
+                    return vakRepository.save(newVak);
+                });
+    }
+
+    @DeleteMapping("vak/{id}")
+    void deleteVak(@PathVariable Integer id){
+        vakRepository.deleteById(id);
     }
 }
